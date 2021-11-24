6e51e0924960a83efde23388c414ada508e1f082
bronx92 <especialcontas@outlook.com>
2021-11-24 00:27:44 -0300

    Adiciona pagina de contato

--- front/blog/src/app/app-routing.module.ts
+++ front/blog/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContatoComponent } from './pages/contato/contato.component';
 import { FeedComponent } from './pages/feed/feed.component';
 import { HomeComponent } from './pages/home/home.component';
 
@@ -12,6 +13,9 @@ const routes: Routes = [
   },
   {
     path: 'feed', component: FeedComponent
+  },
+  {
+    path: 'contato', component: ContatoComponent
   }
 ];
 
--- front/blog/src/app/shared/components/footer/footer.component.ts
+++ front/blog/src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { faLinkedinIn, faGithub} from '@fortawesome/free-brands-svg-icons';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-footer',
@@ -6,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent implements OnInit {
+
+  faLinkedin = faLinkedinIn
+  faGithub = faGithub
+  faGlobe = faGlobe
 
   constructor() { }
 
